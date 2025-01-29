46855a1006092cd49219ab680df38181669d36de
Esther Ikechukwu <estherik18@gmail.com>
2025-01-29 11:35:09 +0100

    JavaScript

---
+++ person.js
@@ -0,0 +1,12 @@
+const person = {
+	firstName : "Esther",
+	lastName : "Ikechukwu",
+	age : 18
+}
+
+function user(person) {
+	return person.firstName +" "+ person.lastName +" "+ person.age
+}
+
+user().toString()
+
